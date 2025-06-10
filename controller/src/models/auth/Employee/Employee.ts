325f5f69c56f47f5d5111ebf8e349f62676ea96f
Pedro Beltran <pebeltranhe@gmail.com>
2025-06-09 22:36:17 -0500

    feat(model/auth/*): Creating employee entity, routes and controllers

---
+++ controller/src/models/auth/Employee/Employee.ts
@@ -0,0 +1,49 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
+import { DocType } from '../DocType/DocType';
+import { EmployeeRole } from '../EmployeeRole/EmployeeRole';
+import { Contract } from '../Contract/Contract';
+
+@Entity({ schema: 'auth', name: 'employee' })
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  lastName!: string;
+
+  @Column()
+  secondLastName!: string;
+  
+  @Column({unique: true})
+  email!: string;
+
+  @Column()
+  phone!: string;
+  
+  @ManyToOne(()=> DocType, (docType) => docType.employees)
+  @JoinColumn({ name: 'docType_id' })
+  docType!: DocType;
+  
+  @Column()
+  docNum!: number;
+  
+  @ManyToOne(()=> EmployeeRole, (employeeRole) => employeeRole.employees)
+  @JoinColumn({ name: 'employeeRole_id' })
+  employeeRole!: EmployeeRole;
+
+  @OneToOne(() => Contract, contract => contract.employee)
+  constract!: Contract;
+  
+  @Column()
+  employeeCode!: string;
+
+  @CreateDateColumn({type: 'timestamp with time zone', default: () => 'NOW()'})
+  created_at!: Date;
+
+  @UpdateDateColumn({type: 'timestamp with time zone', default: () => 'NOW()'})
+  updated_at!: Date;  
+};
+
