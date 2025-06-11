1de9b5f3d4d9704f6eb26adb8c9601592cb186f7
Pedro Beltran <pebeltranhe@gmail.com>
2025-06-11 18:32:36 -0500

    feat(model/core/*): Applying http request methods to role user table

--- controller/src/routers/core/userRole.route.ts
+++ controller/src/routers/core/userRole.route.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import {userRoleController} from '../../controller/core';
 
-const {getRolesTypes} = userRoleController;
+const {getRolesTypes, getRoleType, createRoleType, updateRoleType, deleteRoleType} = userRoleController;
 
 const router = Router();
 
 router.get('/', getRolesTypes);
+router.get('/:id', getRoleType);
+router.post('/', createRoleType);
+router.patch('/:id', updateRoleType);
+router.delete('/:id', deleteRoleType);
 
 export default router;
