69e822b347e14e44b355cbe9848c1874bd48c7ca
Pedro Beltran <pebeltranhe@gmail.com>
2025-06-11 19:45:06 -0500

    feat(model/core/*): Applying http request methods to Company table

--- controller/src/controller/core/company.controller.ts
+++ controller/src/controller/core/company.controller.ts
@@ -6,10 +6,122 @@ class CompanyController {
   public async getCompanies(req: Request, res: Response) : Promise<void> {
     try {
       const repo = AppSource.getRepository(Company);
-      const companies = await repo.find();
+      const companies = await repo.find({
+        where: { is_active: true },
+      });
       res.status(200).json({
         response: companies,
         message: `Company data fetched successfully`
+      });
+    } catch (error) {
+      console.error(`Error fetching Company data:`, error);
+      res.status(500).json({ message: 'Internal Server Error' });    
+    };
+  }
+
+  public async getCompany(req: Request, res: Response) : Promise<void> {
+    const id = +req.params.id;
+
+    if (isNaN(id)) {
+      res.status(400).json({ error: "Invalid ID" })
+      return;
+    };
+
+    try {
+      const repo = AppSource.getRepository(Company);
+
+      const response = await repo.findOne({
+        where: { id, is_active: true }
+      });
+
+      if (!response) {
+        res.status(404).json({ message: "Company was not found" });
+        return;
+      };
+
+      res.status(200).json({
+        response,
+        message: `Company data fetched successfully`
+      });
+    } catch (error) {
+      console.error(`Error fetching Company data:`, error);
+      res.status(500).json({ message: 'Internal Server Error' });    
+    };
+  }
+  
+  public async createCompany(req: Request, res: Response) : Promise<void> {
+    try {
+      const repo = AppSource.getRepository(Company);
+
+      const newCompany = repo.create(req.body);
+      await repo.save(newCompany);
+      
+      res.status(201).json({
+        message: `Company created successfully`
+      });
+    } catch (error) {
+      console.error(`Error fetching Company data:`, error);
+      res.status(500).json({ message: 'Internal Server Error' });    
+    };
+  }
+
+  public async updateCompany(req: Request, res: Response) : Promise<void> {
+    const id = +req.params.id;
+
+    if (isNaN(id)) {
+      res.status(400).json({ error: "Invalid ID" })
+      return;
+    };
+
+    try {
+      const { name } = req.body;
+
+      const repo = AppSource.getRepository(Company);
+
+      const company = await repo.findOneBy({ id });
+
+      if (!company) {
+        res.status(404).json({ message: `Company with id ${id} not found` });
+        return;
+      };
+
+      repo.merge(company, req.body);
+
+      await repo.save(company);
+      
+      res.status(200).json({
+        message: `Company updated successfully`        
+      });
+    } catch (error) {
+      console.error(`Error fetching Company data:`, error);
+      res.status(500).json({ message: 'Internal Server Error' });    
+    };
+  }
+
+  public async deleteCompany(req: Request, res: Response) : Promise<void> {
+    const id = +req.params.id;
+
+    if (isNaN(id)) {
+      res.status(400).json({ error: "Invalid ID" })
+      return;
+    };
+
+    try {
+      const repo = AppSource.getRepository(Company);
+
+      const company = await repo.findOneBy({ id });
+
+      if (!company) {
+        res.status(404).json({ message: `Company with id ${id} not found` });
+        return;
+      };
+
+      repo.merge(company, { is_active: false });
+
+      await repo.save(company);
+      
+      res.status(200).json({
+        message: `Company deleted successfully`  
       });
     } catch (error) {
       console.error(`Error fetching Company data:`, error);
--- controller/src/routers/core/company.route.ts
+++ controller/src/routers/core/company.route.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import {companyController} from '../../controller/core';
 
-const {getCompanies} = companyController;
+const {getCompanies, getCompany, createCompany, updateCompany, deleteCompany} = companyController;
 
 const router = Router();
 
 router.get('/', getCompanies);
+router.get('/:id', getCompany);
+router.post('/', createCompany);
+router.patch('/:id', updateCompany);
+router.delete('/:id', deleteCompany);
 
 export default router;
