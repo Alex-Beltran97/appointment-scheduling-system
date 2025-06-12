4ae5e252888bf898fe00da098a5cb6744844d825
Pedro Beltran <pebeltranhe@gmail.com>
2025-06-11 19:04:44 -0500

    feat(model/core/*): Applying http request methods to Employee Role table

--- controller/src/routers/core/employeeRole.route.ts
+++ controller/src/routers/core/employeeRole.route.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import {employeeRoleController} from '../../controller/core';
 
-const {getEmployeesRoles} = employeeRoleController;
+const {getEmployeesRoles, getEmployeeRole, createEmployeeRole, updateEmployeeRole, deleteEmployeeRole} = employeeRoleController;
 
 const router = Router();
 
 router.get('/', getEmployeesRoles);
+router.get('/:id', getEmployeeRole);
+router.post('/', createEmployeeRole);
+router.patch('/:id', updateEmployeeRole);
+router.delete('/:id', deleteEmployeeRole);
 
 export default router;
