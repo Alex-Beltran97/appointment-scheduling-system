d0150a44aa1a427a37c76f5dc3029be36303ebe9
Pedro Beltran <pebeltranhe@gmail.com>
2025-06-14 07:57:57 -0500

    feat(view/src/*): Applying route services and creating pages for unauthenticated user

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,12 +1,26 @@
 import Header from "./layout/Header/Header";
-import Main from "./layout/Main/Main";
+// import Main from "./layout/Main/Main";
 import Footer from "./layout/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+// import ProtectedRoute from "./routes/ProtectedRoute";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (<>
     <div className="app-container">
       <Header />
-      <Main />
+      <Routes>
+        {['/', '/home'].map((path) => (
+          <Route key={path} path={path} element={<HomePage />} />
+        ))}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Main /> */}
       <Footer />
     </div>
   </>)
--- view/src/components/Header/NavBar/NavBar.tsx
+++ view/src/components/Header/NavBar/NavBar.tsx
@@ -1,14 +1,34 @@
-import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
+import { Home } from '@mui/icons-material';
 
 import styles from './NavBar.module.css';
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path = '') => navigate(`/${path}`, { replace: true });
+
   return (<>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar className={styles.container}>
-          <Button color="inherit">Iniciar Sesion</Button>
-          <Button color="inherit">Registrarse</Button>
+          <IconButton
+            color="inherit"
+            onClick={() => handleNavigate('home')}
+          >
+            <Home />
+          </IconButton>
+          <Box>
+            <Button
+              color="inherit"
+              onClick={() => handleNavigate('login')}
+            >Iniciar Sesion</Button>
+            <Button
+              color="inherit"
+              onClick={() => handleNavigate('register')}
+            >Registrarse</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
